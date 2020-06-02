ae359fc523b417be15188cdb409e72d288edca57
Alexandro Trevisan <alexbaixista@gmail.com>
2020-06-02 18:46:01 -0300

    test files

---
+++ Makefile
@@ -0,0 +1,43 @@
+#EXECUTABLE NAME
+TARGET := game.exe
+
+##COMPILER
+CC := g++
+LD := g++
+
+##FLAGS
+CFLAGS:= -std=c++1y -O3 -Wall
+
+#BUILD FOLDERS
+MODULES := engine game
+SRC_DIR := $(addprefix src/,$(MODULES))
+BUILD_DIR := $(addprefix build/,$(MODULES))
+
+SRC := $(foreach sdir,$(SRC_DIR),$(wildcard $(sdir)/*.cpp))
+OBJ := $(patsubst src/%.cpp,build/%.o,$(SRC))
+INCLUDES := $(addprefix -I,$(SRC_DIR))
+
+
+vpath %.cpp $(SRC_DIR)
+
+define make-goal
+$1/%.o: %.cpp
+	$(CC) $(CFLAGS) $(INCLUDES) -c $$< -o $$@
+endef
+
+.PHONY: all checkdirs clean
+
+all: checkdirs build/$(TARGET)
+
+build/$(TARGET): $(OBJ)
+	$(LD) $^ -o $@
+
+checkdirs: $(BUILD_DIR)
+
+$(BUILD_DIR):
+	@mkdir -p $@
+
+clean:
+	@rm -rf $(BUILD_DIR)
+
+$(foreach bdir,$(BUILD_DIR),$(eval $(call make-goal,$(bdir))))
---
+++ src/game/main.cpp
@@ -0,0 +1,73 @@
+#include <vector>
+#include <memory>
+
+struct Vector3
+{
+    Vector3(const Vector3&) = delete;
+    Vector3():x(0), y(0), z(0){};
+    Vector3(float x, float y, float z)
+    :x(x), y(y), z(z){};
+    float x, y, z;
+};
+
+struct Color
+{
+    Color(const Color&) = delete;
+    static constexpr int blue = 3;
+};
+class Component
+{
+    public:
+    Component(){};
+    Component(const Component&) = delete;
+};
+class Light: public Component
+{
+    public:
+    Light():color(0){};
+    Light(const Light&) = delete;
+    int color;
+};
+
+class GameObject
+{
+    public:
+    GameObject(const std::string& _name)
+    :name(_name){}
+    
+    template<class T>
+    constexpr T& AddComponent()
+    {
+        objects[currentObjIdx] = T();
+        auto* object = &objects[currentObjIdx++];
+        return *static_cast<T*>(object);
+    }
+
+    struct transform
+    {
+        Vector3 position;
+    }transform;
+
+    //private:
+    std::array<Component, 16> objects;
+    uint8_t currentObjIdx = 0;
+    std::string name;
+    
+};
+
+int main()
+{
+    // Make a game object
+    GameObject lightGameObject("The Light123412");// = new GameObject("The Light");
+
+    // Add the light component
+    auto& lightComp = lightGameObject.AddComponent<Light>();
+
+    // Set color and position
+    lightComp.color = Color::blue;
+
+    // Set the position (or any transform property)
+    lightGameObject.transform.position = Vector3(0, 5, 0);
+    
+    return sizeof(std::vector<Component>);
+}
