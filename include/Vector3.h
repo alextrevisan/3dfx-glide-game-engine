6fd180436d7907a354b31a69a72381491c40439d
Desktop\Alex <alexbaixista@gmail.com>
2020-06-17 00:08:24 -0300

    Update Vector3 to return Vertex

--- include/Vector3.h
+++ include/Vector3.h
@@ -1,8 +1,23 @@
 #ifndef __VECTOR3_H__
 #define __VECTOR3_H__
+
+#include <cstdint>
+#include <Vertex.h>
+
+
 struct Vector3
 {
+    Vector3(float a, float b, float c)
+        :x(a), y(b), z(c)
+    {}
+
     float x, y, z;
+
+    Vertex toVertex() const
+    {
+        if(z != 0.0f)
+        return {x/z, y/z, 1.0f/z,0,0,0,0,0};
+    }
 };
 
 #endif //__VECTOR3_H__
